30605491b9ffe9a5087cdb58ff64cc522120b12e
Donghee Kim <dongheekim@woowahan.com>
2022-06-03 20:17:07 +0900

    feat: 코드 포맷팅 자동 적용

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -1,16 +1,16 @@
 val trim = """[^.\d-+*/]""".toRegex()
 
-fun trim(v: String):String = v.replace(trim, "")
+fun trim(v: String): String = v.replace(trim, "")
 
-fun repMtoPM(v:String):String = v.replace("-", "+-")
+fun repMtoPM(v: String): String = v.replace("-", "+-")
 
 val groupMD = """((?:\+-?)?[.\d]+)([*/])((?:\+-?)?[.\d]+)""".toRegex()
 
-fun foldGroup(v:String): Double = groupMD.findAll(v).fold(0.0){acc, curr ->
+fun foldGroup(v: String): Double = groupMD.findAll(v).fold(0.0) { acc, curr ->
     val (_, left, op, right) = curr.groupValues
     val leftValue = left.replace("+", "").toDouble()
     val rightValue = right.replace("+", "").toDouble()
-    val result = when(op) {
+    val result = when (op) {
         "*" -> leftValue * rightValue
         "/" -> leftValue / rightValue
         else -> throw Throwable("Invalid operator $op")
