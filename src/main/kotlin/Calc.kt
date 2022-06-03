9a1a87adeb296396c52c1ad2da6e32368a2e4dc1
Donghee Kim <dongheekim@woowahan.com>
2022-06-03 20:30:53 +0900

    feat: 문자열 젤 처음의 + 기호를 제거한다

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -6,6 +6,13 @@ fun repMtoPM(v: String): String = v.replace("-", "+-")
 
 val groupMD = """((?:\+-?)?[.\d]+)([*/])((?:\+-?)?[.\d]+)""".toRegex()
 
+fun cut(v: String): String = if (v[0] == '+') {
+    v.substring(1)
+} else {
+    v
+}
+
+
 fun foldGroup(v: String): Double = groupMD.findAll(v).fold(0.0) { acc, curr ->
     val (_, left, op, right) = curr.groupValues
     val leftValue = left.replace("+", "").toDouble()
@@ -19,4 +26,13 @@ fun foldGroup(v: String): Double = groupMD.findAll(v).fold(0.0) { acc, curr ->
     acc + result
 }
 
-fun calc(v: String) = foldGroup(repMtoPM(trim(v)))
+
+fun calc(v: String) {
+    val str = cut(repMtoPM(trim(v)))
+
+//    println(str)
+
+
+
+    foldGroup(str)
+}
