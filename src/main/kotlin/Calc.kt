3dbd4118bf2db055000be9e82a9af7ad87828b19
Donghee Kim <dongheekim@woowahan.com>
2022-06-05 13:19:08 +0900

    feat: 음수, 괄호 없는 사칙연산 대응 완료

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -6,13 +6,8 @@ fun repMtoPM(v: String): String = v.replace("-", "+-")
 
 fun cut(v: String): String = if (v[0] == '+') v.substring(1) else v
 
-val expression = """^\+?(-?[.\d]+)""".toRegex()
-
-fun parseExpression(v: String) = expression.find(v)?.groupValues?.get(1)
-
-val operator = """^[+/*]""".toRegex()
-
-fun parseOperator(v: String) = operator.find(v)?.value
+val re = """^(-?[.\d]+|[+/*])""".toRegex()
+fun parse(v: String) = re.find(v)
 
 fun eval(op: String, leftValue: Double, rightValue: Double): Double = when (op) {
     "+" -> leftValue + rightValue
@@ -21,25 +16,30 @@ fun eval(op: String, leftValue: Double, rightValue: Double): Double = when (op) 
     else -> throw Throwable("Invalid operator $op")
 }
 
-fun calc(v: String): Double = _calc(cut(repMtoPM(trim(v))))
+fun calc(v: String): Double {
+    var str = cut(repMtoPM(trim(v)))
+    val next = {
+        val (all, value) = parse(str)?.groupValues ?: throw Throwable("Invalid string $str")
+        str = str.substring(all.length)
+        value
+    }
 
-fun _calc(v: String): Double {
-    var str = v
+    return f(next, next().toDouble(), next())
+}
 
-    val exp = parseExpression(str) ?: return 0.0
-    str = str.substring(exp.length)
-    val exp1 = exp.toDouble()
+fun f(next: () -> String, curr: Double, op: String): Double {
+    val n = next().toDouble()
 
-    if (str == "") return exp1
+    val nextOp: String
+    try {
+        nextOp = next()
+    } catch (e: Throwable) {
+        return eval(op, curr, n)
+    }
 
-    val op = parseOperator(str) ?: throw Throwable("Invalid operator $str")
-    str = str.substring(op.length)
-
-    val _exp = parseExpression(str) ?: throw Throwable("Invalid expression $str")
-    str = str.substring(exp.length)
-    val exp2 = _exp.toDouble()
-
-    println("$op $exp1 $exp2")
-
-    return eval(op, exp1, exp2)
+    return if (op == "+" && nextOp != "+") {
+        eval(op, curr, f(next, n, nextOp))
+    } else {
+        f(next, eval(op, curr, n), nextOp)
+    }
 }
