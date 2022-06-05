a68a75d0f7d052ed4b2136024010cbddc99cc58b
Donghee Kim <dongheekim@woowahan.com>
2022-06-05 14:06:53 +0900

    feat: MtoPM 을 정규표현식으로 변경한다

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -2,11 +2,12 @@ val trim = """[^.\d-+*/]""".toRegex()
 
 fun trim(v: String): String = v.replace(trim, "")
 
-fun repMtoPM(v: String): String = v.replace("-", "+-").replace("*+", "*").replace("/+", "/")
+val MtoPM = """(?<!^|[*/])-""".toRegex()
 
-fun cut(v: String): String = if (v[0] == '+') v.substring(1) else v
+fun repMtoPM(v: String): String = v.replace(MtoPM, "+-")
 
 val re = """^(-?[.\d]+|[+/*])""".toRegex()
+
 fun parse(v: String) = re.find(v)
 
 fun eval(op: String, leftValue: Double, rightValue: Double): Double = when (op) {
@@ -17,7 +18,7 @@ fun eval(op: String, leftValue: Double, rightValue: Double): Double = when (op) 
 }
 
 fun calc(v: String): Double {
-    var str = cut(repMtoPM(trim(v)))
+    var str = repMtoPM(trim(v))
     val next = {
         val (all, value) = parse(str)?.groupValues ?: throw Throwable("Invalid string $str")
         str = str.substring(all.length)
